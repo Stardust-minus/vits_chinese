adc95227f7c3ede0a29f57e235ac9b777d0e75dd
Stardust·减 <2225664821@qq.com>
2023-05-29 07:05:17 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -398,7 +398,7 @@ def evaluate(hps, generator, eval_loader, writer_eval):
             y_lengths = y_lengths[:1]
             speakers = speakers[:1]
             break
-        y_hat, attn, mask, *_ = generator.module.infer(x, x_lengths, speakers, bert, max_len=1000)
+        y_hat, attn, mask, *_ = generator.module.infer(x, x_lengths, bert, speakers, max_len=1000)
         y_hat_lengths = mask.sum([1, 2]).long() * hps.data.hop_length
 
         mel = spec_to_mel_torch(
