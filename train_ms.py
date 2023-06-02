a355ce2913d05aa3144089562eeb83cc89d3485e
Stardust·减 <2225664821@qq.com>
2023-06-02 18:23:25 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -139,12 +139,18 @@ def run(rank, n_gpus, hps):
     net_d = DDP(net_d, device_ids=[rank])
 
     try:
-        _, _, _, epoch_str = utils.load_checkpoint(
-            utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g
-        )
-        _, _, _, epoch_str = utils.load_checkpoint(
-            utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d, optim_d
-        )
+        if ckptG is not None:
+            _, _, _, epoch_str = utils.load_checkpoint(ckptG, net_g, optim_g, is_old=True)
+            print("加载原版VITS模型G记录点成功")
+        else:
+            _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g,
+                                                   optim_g)
+        if ckptD is not None:
+            _, _, _, epoch_str = utils.load_checkpoint(ckptG, net_g, optim_g, is_old=True)
+            print("加载原版VITS模型D记录点成功")
+        else:
+            _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d,
+                                                   optim_d)
         global_step = (epoch_str - 1) * len(train_loader)
     except:
         epoch_str = 1
